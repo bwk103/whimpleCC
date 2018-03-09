eb066436be9cf3ebaa996bd85d0f2ee245d8401b
Ben Kielty <bwk103@gmail.com>
2018-03-09 16:31:38 +0000

    Basic UI for Players#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@ leigh = Player.create!(first_name: 'Leigh',
                        surname: 'Lorenzen',
                        email: 'leigh@test.com',
                        team: 1)
+tom = Player.create!(first_name: 'Tom',
+                      surname: 'Allan',
+                      email: 'tom@test.com',
+                      team: 1)
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
