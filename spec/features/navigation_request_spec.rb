1906b92c6c0ec6605e45bb95f51ad65b0ba620f4
Ben Kielty <bwk103@gmail.com>
2018-03-09 15:47:19 +0000

    Add Basic Styling to User#Index
    Added basic styling to /players path.

--- spec/features/navigation_request_spec.rb
+++ spec/features/navigation_request_spec.rb
@@ -10,4 +10,13 @@ RSpec.feature "Navbar navigation", :type => :feature do
     expect(page).to have_link("More")
   end
 
+  scenario "User can navigate to /players" do
+    visit '/'
+    expect(page).to have_link("Squads")
+    click_link "Squads"
+    expect(page.current_path).to eq players_path
+    click_link "Whimple CC"
+    expect(page.current_path).to eq root_path
+  end
+
 end
