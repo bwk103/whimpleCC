a2913bea52c73a7b81d81bae3c6c4545aaec3176
Ben Kielty <bwk103@gmail.com>
2018-04-19 12:01:39 +0100

    Finishing batting stats validators

--- app/models/batting.rb
+++ app/models/batting.rb
@@ -1,7 +1,24 @@
 class Batting < ApplicationRecord
+  attr_accessor :average
+
   validates :games, numericality: { greater_than_or_equal_to: 0 }
   validates :innings, numericality: {
-                        greater_than_or_equal_to: 0,
-                        less_than_or_equal_to: proc { |stat| stat.games * 2 }
-                      }
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: proc { |stat| stat.games * 2 }
+  }
+  validates :not_outs, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: proc { |stat| stat.games * 2 }
+  }
+  validates :runs, numericality: { greater_than_or_equal_to: 0 }
+  validates :high_score, numericality: { greater_than_or_equal_to: 0,
+                                         less_than_or_equal_to: :runs }
+  validates :average, numericality: { greater_than_or_equal_to: 0 }
+  validates :fifties, numericality: { greater_than_or_equal_to: 0 }
+  validates :hundreds, numericality: { greater_than_or_equal_to: 0 }
+
+  def set_average
+    return self.average = nil if self.innings == self.not_outs
+    self.average = runs / (innings - not_outs)
+  end
 end
--- spec/models/batting_spec.rb
+++ spec/models/batting_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Batting, type: :model do
       not_outs: 1,
       runs: 55,
       high_score: 34,
-      average: 34,
+      average: 55,
       fifties: 0,
       hundreds: 0
     )
@@ -37,6 +37,73 @@ RSpec.describe Batting, type: :model do
         @bat.innings = 3
         expect(@bat).not_to be_valid
       end
+    end
+
+    describe('not_outs') do
+      it('cannot be less than 0') do
+        @bat.not_outs = -1
+        expect(@bat).not_to be_valid
+      end
+      it('cannot be more than games played x 2') do
+        @bat.not_outs = 3
+        expect(@bat).not_to be_valid
+      end
+    end
+
+    describe('runs') do
+      it('cannot be negative') do
+        @bat.runs = -1
+        expect(@bat).not_to be_valid
+      end
+    end
+
+    describe('high score') do
+      it('cannot be negative') do
+        @bat.high_score = -1
+        expect(@bat).not_to be_valid
+      end
+      it('cannot be higher than total runs scored') do
+        @bat.high_score = 100
+        @bat.set_average
+        expect(@bat).not_to be_valid
+      end
+    end
+
+    describe('average') do
+      it('cannot be negative') do
+        @bat.average = -1
+        expect(@bat).not_to be_valid
+      end
+    end
+
+    describe('fifties') do
+      it('cannot be negative') do
+        @bat.fifties = -1
+        expect(@bat).not_to be_valid
+      end
+    end
+
+    describe('hundreds') do
+      it('cannot be negative') do
+        @bat.hundreds = -1
+        expect(@bat).not_to be_valid
+      end
+    end
+  end
+
+  describe('#set_average') do
+    it('sets the batsman\'s average score') do
+      expect(@bat.average).to be 55
+      @bat.runs = 100
+      @bat.not_outs = 0
+      @bat.set_average
+      expect(@bat.average).to be 50
+    end
+
+    it("catches the situation where batsman has never been out") do
+    @bat.not_outs = 2
+    @bat.set_average
+    expect(@bat.average).to be_nil
     end
   end
 end
