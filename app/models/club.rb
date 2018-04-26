24bff3be741de2a89db1a23e4c7e51bc88409f69
Ben Kielty <bwk103@gmail.com>
2018-04-26 09:23:32 +0100

    Added validators to Club name property

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,3 @@
 class Club < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 end
--- spec/models/club_spec.rb
+++ spec/models/club_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Club, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @club = Club.new(name: "Cricket Club", ground: "Cricket Ground")
+  end
+
+  describe 'basics' do
+    it 'is valid' do
+      expect(@club).to be_valid
+    end
+  end
+  describe 'validators' do
+    describe 'name' do
+      it 'must not be nil' do
+        @club.name = nil
+        expect(@club).not_to be_valid
+      end
+      it 'must be at least three characters long' do
+        @club.name = 'ab'
+        expect(@club).not_to be_valid
+      end
+      it 'must be no longer than 50 characters long' do
+        @club.name = 'a' * 51
+        expect(@club).not_to be_valid
+      end
+    end
+  end
 end
