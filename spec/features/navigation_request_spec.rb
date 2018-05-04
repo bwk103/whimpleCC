24de89378aae593f4490d7cdad9c927fcf161ead
Ben Kielty <bwk103@gmail.com>
2018-05-04 10:12:52 +0100

    Minor Amends to frontend

--- spec/features/navigation_request_spec.rb
+++ spec/features/navigation_request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Navbar navigation', type: :feature do
   scenario 'User can see navbar' do
     visit '/'
-    expect(page).to have_link('Club')
     expect(page).to have_link('Squads')
     expect(page).to have_link('Fixtures')
     expect(page).to have_link('Events')
