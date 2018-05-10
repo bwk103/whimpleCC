f5fc469f6b62be9998c1749ba423e895bbdd8470
Ben Kielty <bwk103@gmail.com>
2018-05-10 12:30:36 +0100

    Update NavBar
    Updated Navbar layout to link to fixtures#index.
    Added unit tests for navigation to history, events and fixtures.

--- spec/features/navigation_request_spec.rb
+++ spec/features/navigation_request_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Navbar navigation', type: :feature do
   scenario 'User can see navbar' do
     visit '/'
+    expect(page).to have_link('History')
     expect(page).to have_link('Players')
     expect(page).to have_link('Fixtures')
     expect(page).to have_link('Events')
@@ -16,5 +17,26 @@ RSpec.feature 'Navbar navigation', type: :feature do
     expect(page.current_path).to eq players_path
     click_link 'Whimple CC'
     expect(page.current_path).to eq root_path
+  end
+
+  scenario 'User can navigate to fixtures' do
+    visit '/'
+    expect(page).to have_link 'Fixtures'
+    click_link 'Fixtures'
+    expect(page.current_path).to eq fixtures_path
+  end
+
+  scenario 'User can navigate to History' do
+    visit '/'
+    expect(page).to have_link 'History'
+    click_link 'History'
+    expect(page.current_path).to eq history_path
+  end
+
+  scenario 'User can navigate to Events' do
+    visit '/'
+    expect(page).to have_link 'Events'
+    click_link 'Events'
+    expect(page.current_path).to eq events_path
   end
 end
